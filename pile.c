2121fe7435876c69c3654bbf286c1110dead2534
leroux <leroux.leo01@gmail.com>
2021-03-25 08:41:06 +0100

    8:41

--- pile.c
+++ pile.c
@@ -4,52 +4,84 @@
 
 void initPile( T_Pile * P)
 {
+    P->nbElts=0;
 }
 
 
 int pilepleine(const  T_Pile *P)
 {
-return 0;
+    if (P->nbElts==MAX){
+        return 1;
+    }
+    else return 0;
 }
 
 
 
 int pilevide(const  T_Pile *P)
 {
-return 1;
+    if (P->nbElts==0){
+        return 1;
+    }
+    else return 0;
 }
 
 
 
 int empiler( T_Pile *P, T_Elt e) //renvoie 0 si pile pleine, sinon 1
 {
-return 0;
+    if (pilepleine(P)==1)
+    {
+        printf("Pile pleine, impossible d'empiler un nouvel élèment");
+        return 0;
+    }
+    else
+    {
+        printf("Saissez l'élèment à empiler");
+        saisirElt(e);
+        P->nbElts = P->nbElts +1;
+        return 1;
+    }
 }
 
 
 
 int depiler( T_Pile *P, T_Elt *pelt)  //renvoie 0 si pile vide, sinon 1
 {
-return 0;
+    if (pilevide(P)==1)
+    {
+        printf("Pile vide, impossible de dépiler élèment");
+        return 0;
+    }
+    else
+    {
+        affecterElt(pelt,P->Elts[(P->nbElts)-1]);
+        P->nbElts = P->nbElts - 1;
+        return 1;
+    }
 }
 
 
 
 T_Elt sommet(const  T_Pile *P)
 {
-return 0;
+return P->Elts[(P->nbElts) - 1];
 }
 
 
 
 int hauteur(const  T_Pile *P)
 {
-return 0;
+return P->nbElts;
 }
 
 
 void afficherPile(  T_Pile *P)
 {
+    for (int i=0;i<(P->nbElts);i++)
+    {
+        afficherElt(P->Elts[i]);
+    }
 }
 
 
